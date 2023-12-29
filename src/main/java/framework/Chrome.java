26183b0a5621cbcab8a0a32bb1f43cb523891e09
Femina Jamal <git@gitlab.com>
2023-12-29 17:26:09 +0530

    pushing

--- src/main/java/framework/Chrome.java
+++ src/main/java/framework/Chrome.java
@@ -2,13 +2,18 @@ package framework;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Chrome implements DriverStrategy {
 
     @Override
     public WebDriver setStrategy() {
-        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        return driver;
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("useAutomationExtension", false);
+        options.addArguments("--no-sandbox");
+        return new ChromeDriver(options);
+        //return driver;
     }
 }
